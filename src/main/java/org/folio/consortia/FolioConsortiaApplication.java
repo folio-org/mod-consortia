a9c198e70b8cf6e4508e32d3f886b632d403e413
Adesh_Singh <Adesh_Singh@epam.com>
2023-03-06 21:38:30 +0530

    [MODFISTO-399]-Added endpoint to get List of tenants.

--- src/main/java/org/folio/consortia/FolioConsortiaApplication.java
+++ src/main/java/org/folio/consortia/FolioConsortiaApplication.java
@@ -1,4 +1,4 @@
-package org.folio;
+package org.folio.consortia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/org/folio/consortia/FolioConsortiaApplicationTest.java
+++ src/test/java/org/folio/consortia/FolioConsortiaApplicationTest.java
@@ -1,4 +1,4 @@
-package org.folio;
+package org.folio.consortia;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
