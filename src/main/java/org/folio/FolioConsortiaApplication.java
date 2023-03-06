06d8d4eae785e13e77671eef2d569c0b40c1aa20
Adesh_Singh <Adesh_Singh@epam.com>
2023-03-06 14:48:55 +0530

    [MODFISTO-399]-Added endpoint to get List of tenants.

--- src/main/java/org/folio/FolioConsortiaApplication.java
+++ src/main/java/org/folio/FolioConsortiaApplication.java
@@ -2,10 +2,12 @@ package org.folio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableFeignClients
 @SpringBootApplication
+@EnableJpaRepositories
+@EntityScan("org.folio.entity")
 public class FolioConsortiaApplication {
 
   public static void main(String[] args) {
