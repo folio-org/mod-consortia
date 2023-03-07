502aa6c855c94dff0d00267fcd207dbc3c8b5e5c
Adesh_Singh <Adesh_Singh@epam.com>
2023-03-07 16:58:11 +0530

    [MODFISTO-399]-Added endpoint to get List of tenants.

--- src/main/java/org/folio/consortia/repository/entity/Tenant.java
+++ src/main/java/org/folio/consortia/repository/entity/Tenant.java
@@ -2,15 +2,13 @@ package org.folio.consortia.repository.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
 
-@Entity
 @Data
+@Entity
 public class Tenant {
   @Id
-  @GeneratedValue
   @Column(name = "tenant_id", updatable = false, nullable = false, unique = true)
   private String id;
   private String tenantName;
