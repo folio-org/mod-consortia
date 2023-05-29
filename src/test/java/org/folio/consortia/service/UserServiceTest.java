094746760688b631b99bc87ef04dfd39b669e6cd
Azizbek <113523904+azizbekxm@users.noreply.github.com>
2023-05-29 18:54:15 +0500

    [MODCON-43] - Create system user to support all actions during Consortia tenant setup

--- src/test/java/org/folio/consortia/service/UserServiceTest.java
+++ src/test/java/org/folio/consortia/service/UserServiceTest.java
@@ -1,19 +1,12 @@
 package org.folio.consortia.service;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.when;
-
-import java.util.UUID;
-
 import org.folio.consortia.client.UsersClient;
-import org.folio.consortia.config.FolioExecutionContextHelper;
 import org.folio.consortia.domain.dto.Personal;
 import org.folio.consortia.domain.dto.User;
 import org.folio.consortia.exception.ResourceNotFoundException;
 import org.folio.consortia.service.impl.UserServiceImpl;
 import org.folio.spring.FolioExecutionContext;
+import org.folio.spring.integration.XOkapiHeaders;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -22,6 +15,15 @@ import org.mockito.Mockito;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @EnableAutoConfiguration(exclude = BatchAutoConfiguration.class)
@@ -32,17 +34,12 @@ class UserServiceTest {
   @Mock
   UsersClient usersClient;
   @Mock
-  FolioExecutionContext folioExecutionContext = new FolioExecutionContext() {
-  };
-  @Mock
-  FolioExecutionContextHelper contextHelper;
+  FolioExecutionContext folioExecutionContext;
 
   @Test
   void shouldCreateUser() {
     User user = createUserEntity(false);
-    Mockito.doNothing()
-      .when(usersClient)
-      .saveUser(user);
+    Mockito.doNothing().when(usersClient).saveUser(user);
     User createdUser = userService.createUser(user);
     Assertions.assertEquals(user, createdUser);
   }
@@ -50,40 +47,36 @@ class UserServiceTest {
   @Test
   void shouldUpdateUser() {
     User user = createUserEntity(false);
-    Mockito.doNothing()
-      .when(usersClient)
-      .updateUser(user.getId(), user);
+    Mockito.doNothing().when(usersClient).updateUser(user.getId(), user);
     Assertions.assertDoesNotThrow(() -> userService.updateUser(user));
   }
 
   @Test
   void shouldDeleteUser() {
     User user = createUserEntity(false);
-    Mockito.doNothing()
-      .when(usersClient)
-      .deleteUser(user.getId());
+    Mockito.doNothing().when(usersClient).deleteUser(user.getId());
     Assertions.assertDoesNotThrow(() -> userService.deleteById(user.getId()));
   }
 
   @Test
   void shouldThrowNotFoundWhilePrepareShadowUser() {
     when(folioExecutionContext.getTenantId()).thenReturn("diku");
-    doReturn(folioExecutionContext).when(contextHelper)
-      .getSystemUserFolioExecutionContext(anyString());
-
-    Mockito.when(usersClient.getUsersByUserId(any()))
-      .thenReturn(new User());
-    Assertions.assertThrows(ResourceNotFoundException.class, () -> userService.prepareShadowUser(UUID.randomUUID(), ""));
+    when(folioExecutionContext.getInstance()).thenReturn(folioExecutionContext);
+    Map<String, Collection<String>> okapiHeaders = new HashMap<>();
+    okapiHeaders.put(XOkapiHeaders.TENANT, List.of("diku"));
+    when(folioExecutionContext.getOkapiHeaders()).thenReturn(okapiHeaders);
+    Mockito.when(usersClient.getUsersByUserId(any())).thenReturn(new User());
+    Assertions.assertThrows(org.folio.consortia.exception.ResourceNotFoundException.class, () -> userService.prepareShadowUser(UUID.randomUUID(), ""));
   }
 
   @Test
   void shouldPrepareShadowUser() {
     when(folioExecutionContext.getTenantId()).thenReturn("diku");
-    doReturn(folioExecutionContext).when(contextHelper)
-      .getSystemUserFolioExecutionContext(anyString());
-
-    Mockito.when(usersClient.getUsersByUserId(any()))
-      .thenReturn(createUserEntity(true));
+    when(folioExecutionContext.getInstance()).thenReturn(folioExecutionContext);
+    Map<String, Collection<String>> okapiHeaders = new HashMap<>();
+    okapiHeaders.put(XOkapiHeaders.TENANT, List.of("diku"));
+    when(folioExecutionContext.getOkapiHeaders()).thenReturn(okapiHeaders);
+    Mockito.when(usersClient.getUsersByUserId(any())).thenReturn(createUserEntity(true));
     User user = userService.prepareShadowUser(UUID.randomUUID(), "diku");
     Assertions.assertEquals(true, user.getActive());
   }
@@ -95,8 +88,7 @@ class UserServiceTest {
     personal.setEmail("Test@mail.com");
     personal.setFirstName("testFirst");
     personal.setLastName("testLast");
-    user.setId(UUID.randomUUID()
-      .toString());
+    user.setId(UUID.randomUUID().toString());
     user.setPatronGroup(null);
     user.setUsername("xyz");
     user.setPersonal(personal);
