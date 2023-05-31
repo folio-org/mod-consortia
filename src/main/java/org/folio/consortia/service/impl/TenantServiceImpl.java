183aefc406eda5e5cc92cad29bfc4262ba851803
Mikita Siadykh <mikita_siadykh@epam.com>
2023-05-31 08:20:56 +0400

    MODCON-42 missed permission

--- src/main/java/org/folio/consortia/service/impl/TenantServiceImpl.java
+++ src/main/java/org/folio/consortia/service/impl/TenantServiceImpl.java
@@ -101,8 +101,8 @@ public class TenantServiceImpl implements TenantService {
 
     try (var context = new FolioExecutionContextSetter(contextHelper.getSystemUserFolioExecutionContext(tenantDto.getId()))) {
       configurationClient.saveConfiguration(createConsortiaConfigurationBody(centralTenantId));
-      createShadowAdminUserWithPermissions(shadowAdminUser);
       createUserTenantWithDummyUser(tenantDto.getId());
+      createShadowAdminUserWithPermissions(shadowAdminUser);
     }
     log.info("save:: saved consortia configuration with centralTenantId={} by tenantId={} context", centralTenantId, tenantDto.getId());
     return savedTenant;
