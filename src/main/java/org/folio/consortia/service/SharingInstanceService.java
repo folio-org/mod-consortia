01e54801bb650e7b9ef019eb5da2872f5ed0b1fc
Azizbek <113523904+azizbekxm@users.noreply.github.com>
2023-06-22 13:46:19 +0500

    [MODCON - 59] - Implement POST endpoint to initiate instance sharing process

--- src/main/java/org/folio/consortia/service/SharingInstanceService.java
+++ src/main/java/org/folio/consortia/service/SharingInstanceService.java
@@ -7,10 +7,10 @@ import org.folio.consortia.domain.dto.SharingInstance;
 public interface SharingInstanceService {
 
   /**
-   * Create Shared Instance action
+   * Create Sharing Instance action
    * @param consortiumId UUID of consortium entity
-   * @param sharedInstance the sharedActionDto
-   * @return SharedInstanceDto
+   * @param sharingInstance the sharingInstanceDto
+   * @return SharingInstanceDto
    */
-  SharingInstance save(UUID consortiumId, SharingInstance sharedInstance);
+  SharingInstance save(UUID consortiumId, SharingInstance sharingInstance);
 }
