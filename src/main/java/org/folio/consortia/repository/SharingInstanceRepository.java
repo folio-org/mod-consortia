417cda884ed23071c06697e382c4f052f579dd42
Bekhzod Kurbonboev <bekhzod_kurbonboev@epam.com>
2023-07-03 23:29:40 +0500

    Refactor 'SharingInstanceRepository' file to remove value type

--- src/main/java/org/folio/consortia/repository/SharingInstanceRepository.java
+++ src/main/java/org/folio/consortia/repository/SharingInstanceRepository.java
@@ -1,11 +1,10 @@
 package org.folio.consortia.repository;
 
-import static java.util.Objects.nonNull;
-import static org.apache.commons.lang3.StringUtils.isNotEmpty;
-
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.UUID;
 
+import org.apache.commons.lang3.StringUtils;
 import org.folio.consortia.domain.dto.Status;
 import org.folio.consortia.domain.entity.SharingInstanceEntity;
 import org.springframework.data.jpa.domain.Specification;
@@ -20,16 +19,16 @@ public interface SharingInstanceRepository extends JpaRepository<SharingInstance
     static Specification<SharingInstanceEntity> constructSpecification(UUID instanceIdentifier, String sourceTenantId,
         String targetTenantId, Status status) {
       var list = new ArrayList<Specification<SharingInstanceEntity>>();
-      if(nonNull(instanceIdentifier)) list.add(by("instanceId", instanceIdentifier, UUID.class));
-      if(isNotEmpty(sourceTenantId)) list.add(by("sourceTenantId", sourceTenantId, String.class));
-      if(isNotEmpty(targetTenantId)) list.add(by("targetTenantId", targetTenantId, String.class));
-      if(nonNull(status)) list.add(by("status", status, Status.class));
+      if(Objects.nonNull(instanceIdentifier)) list.add(by("instanceId", instanceIdentifier));
+      if(StringUtils.isNotEmpty(sourceTenantId)) list.add(by("sourceTenantId", sourceTenantId));
+      if(StringUtils.isNotEmpty(targetTenantId)) list.add(by("targetTenantId", targetTenantId));
+      if(Objects.nonNull(status)) list.add(by("status", status));
 
       return list.stream().reduce(Specification::and).orElse(null);
     }
 
-    static Specification<SharingInstanceEntity> by(String fieldName, Object fieldValue, Class<?> clazz) {
-      return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(fieldName), clazz.cast(fieldValue));
+    static Specification<SharingInstanceEntity> by(String fieldName, Object fieldValue) {
+      return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(fieldName), fieldValue);
     }
   }
 }
