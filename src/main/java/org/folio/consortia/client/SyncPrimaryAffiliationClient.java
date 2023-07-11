dde9bff60896f1bde1c72d9e533a211ef7d50c2e
Azizbek <113523904+azizbekxm@users.noreply.github.com>
2023-07-11 17:38:21 +0500

    [MODCON - 73] - Minor improvements

--- src/main/java/org/folio/consortia/client/SyncPrimaryAffiliationClient.java
+++ src/main/java/org/folio/consortia/client/SyncPrimaryAffiliationClient.java
@@ -12,12 +12,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "consortia", configuration = FeignClientConfiguration.class)
 public interface SyncPrimaryAffiliationClient {
   @PostMapping(value = "/{consortiumId}/tenants/{tenantId}/sync-primary-affiliations")
-  void syncPrimaryAffiliations(@PathVariable("consortiumId") String consortiumId, @PathVariable("tenantId") String tenantId,
-    @RequestParam("centralTenantId") String centralTenantId);
+  void syncPrimaryAffiliations(@PathVariable String consortiumId, @PathVariable String tenantId, @RequestParam String centralTenantId);
 
   @PostMapping(value = "/{consortiumId}/tenants/{tenantId}/create-primary-affiliations", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   SyncPrimaryAffiliationBody savePrimaryAffiliations(@RequestBody SyncPrimaryAffiliationBody syncPrimaryAffiliationBody,
-    @PathVariable("consortiumId") String consortiumId, @PathVariable("tenantId") String tenantId,
-    @RequestParam("centralTenantId") String centralTenantId);
+    @PathVariable String consortiumId, @PathVariable String tenantId, @RequestParam String centralTenantId);
 
 }
