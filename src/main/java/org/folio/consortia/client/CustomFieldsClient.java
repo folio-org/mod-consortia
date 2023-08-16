f83ff5fd4e855e0bc35291ffee61c8850e67d7a9
Adesh_Singh <Adesh_Singh@epam.com>
2023-08-16 20:44:22 +0530

    [MODCON-83]-Intigration with folio-custom-field

--- src/main/java/org/folio/consortia/client/CustomFieldsClient.java
+++ src/main/java/org/folio/consortia/client/CustomFieldsClient.java
@@ -6,10 +6,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "custom-fields", configuration = FeignClientConfiguration.class)
 public interface CustomFieldsClient {
 
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-  void postCustomFields(@RequestBody CustomField entity);
+  void postCustomFields(@RequestHeader(value = "x-okapi-module-id") String moduleId, @RequestBody CustomField entity);
 }
--- src/main/java/org/folio/consortia/service/impl/CustomFieldServiceImpl.java
+++ src/main/java/org/folio/consortia/service/impl/CustomFieldServiceImpl.java
@@ -3,20 +3,42 @@ package org.folio.consortia.service.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.folio.consortia.client.CustomFieldsClient;
+import org.folio.consortia.client.OkapiClient;
 import org.folio.consortia.domain.dto.CustomField;
 import org.folio.consortia.service.CustomFieldService;
+import org.folio.spring.FolioExecutionContext;
+import org.folio.spring.exception.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.net.URI;
+
+import static java.lang.String.format;
 
 @Service
 @Log4j2
 @RequiredArgsConstructor
 public class CustomFieldServiceImpl implements CustomFieldService {
-
+  @Autowired
+  FolioExecutionContext folioExecutionContext;
+  @Autowired
+  OkapiClient okapiClient;
+  public static final String OKAPI_URL = "http://_";
   private final CustomFieldsClient customFieldsClient;
+  private static final String MOD_USERS = "mod-users";
 
   @Override
   public void createCustomField(CustomField customField) {
     log.info("Creating custom-field with name {}.", customField.getName());
-    customFieldsClient.postCustomFields(customField);
+    customFieldsClient.postCustomFields(getModuleId(MOD_USERS), customField);
+  }
+
+  public String getModuleId(String moduleName) {
+    var tenantId = folioExecutionContext.getTenantId();
+    var moduleNamesJson = okapiClient.getModuleIds(URI.create(OKAPI_URL), tenantId, moduleName);
+    if (!moduleNamesJson.isEmpty()) {
+      return moduleNamesJson.get(0).get("id").asText();
+    }
+    throw new NotFoundException(format("Module id not found for name: %s", moduleName));
   }
 }
