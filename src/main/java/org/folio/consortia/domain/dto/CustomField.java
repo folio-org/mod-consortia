941a941e4842e89d3f86e81e99df17025a7ffa09
Adesh_Singh <Adesh_Singh@epam.com>
2023-08-18 22:39:58 +0530

    [MODCON-83]-Intigration with folio-custom-field

--- src/main/java/org/folio/consortia/domain/dto/CustomField.java
+++ src/main/java/org/folio/consortia/domain/dto/CustomField.java
@@ -2,6 +2,7 @@ package org.folio.consortia.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
@@ -12,9 +13,10 @@ import lombok.extern.jackson.Jacksonized;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CustomField {
+  @JsonProperty("type")
+  CustomFieldType customFieldType;
   String name;
   String entityType;
-  CustomFieldType customFieldType;
   String helpText;
   Boolean visible;
 }
