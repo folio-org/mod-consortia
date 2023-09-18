bfa1df54a7140646b011f041e19aa59d7daffe35
Serhii_Nosko <Serhii_Nosko@epam.com>
2023-09-18 11:10:13 +0300

    MODCON-93. Add userId to kafka events

--- src/main/java/org/folio/consortia/config/kafka/KafkaService.java
+++ src/main/java/org/folio/consortia/config/kafka/KafkaService.java
@@ -7,6 +7,7 @@ import static org.folio.consortia.messaging.listener.ConsortiaSharingInstanceEve
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 import org.apache.commons.lang3.StringUtils;
@@ -41,7 +42,6 @@ public class KafkaService {
   private final FolioExecutionContext folioExecutionContext;
   private final KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
   private final FolioKafkaProperties folioKafkaProperties;
-  private final Environment springEnvironment;
   private final String kafkaEnvId;
   private final KafkaTemplate<String, Object> kafkaTemplate;
 
@@ -153,6 +153,9 @@ public class KafkaService {
     producerRecord.headers().add(XOkapiHeaders.TENANT, folioExecutionContext.getTenantId().getBytes(StandardCharsets.UTF_8));
     producerRecord.headers().add(XOkapiHeaders.TOKEN, folioExecutionContext.getToken().getBytes(StandardCharsets.UTF_8));
     producerRecord.headers().add(XOkapiHeaders.URL, folioExecutionContext.getOkapiUrl().getBytes(StandardCharsets.UTF_8));
+    if (Objects.nonNull(folioExecutionContext.getUserId())) {
+      producerRecord.headers().add(XOkapiHeaders.USER_ID, folioExecutionContext.getUserId().toString().getBytes(StandardCharsets.UTF_8));
+    }
     return producerRecord;
   }
 }
