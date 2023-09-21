f480e6fdb105ebd768755d9fe824b1f52755f0f7
Azizbek <113523904+azizbekxm@users.noreply.github.com>
2023-09-21 21:32:27 +0500

    [MODCON-85] - improved default value

--- src/main/java/org/folio/consortia/service/impl/SharingSettingServiceImpl.java
+++ src/main/java/org/folio/consortia/service/impl/SharingSettingServiceImpl.java
@@ -52,9 +52,9 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @RequiredArgsConstructor
 public class SharingSettingServiceImpl implements SharingSettingService {
-  @Value("${folio.time.interval:200}")
+  @Value("${folio.sharing.setting.interval:200}")
   private int interval;
-  @Value("${folio.time.max-tries:5}")
+  @Value("${folio.sharing.setting.max-tries:10}")
   private int maxTries;
 
   private static final String SOURCE = "source";
